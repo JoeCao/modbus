c41942968b288c8f4f2a93c248e438afcdd40e51
JoeCao <caozupeng@139.com>
2024-06-03 09:56:36 +0800

    增加一个main入口作为测试

---
+++ demo/mytest.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/goburrow/modbus"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	my_tcpDevice = "127.0.0.1:502"
+)
+
+func main() {
+	err := Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Read() error {
+	handler := modbus.NewTCPClientHandler(my_tcpDevice)
+	handler.Timeout = 5 * time.Second
+	handler.SlaveId = 1
+	handler.Logger = log.New(os.Stdout, "tcp: ", log.LstdFlags)
+	err := handler.Connect()
+	if err != nil {
+		return fmt.Errorf("failed to connect handler: %w", err)
+	}
+	defer handler.Close()
+	client := modbus.NewClient(handler)
+	results, err := client.ReadHoldingRegisters(1, 1)
+	if err != nil || results == nil {
+		return fmt.Errorf("failed to read holding registers: %w", err)
+	}
+	fmt.Println(results)
+	// Convert the result to int16
+	if len(results) < 2 {
+		return fmt.Errorf("unexpected result length: %d", len(results))
+	}
+	// 创建 Reader
+	r := bytes.NewReader(results)
+	// 读取一个 uint16
+	var u uint16
+	if err := binary.Read(r, binary.BigEndian, &u); err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	fmt.Printf("uint16: %d\n", u)
+	//value := int16(binary.BigEndian.Uint16(results))
+	//fmt.Println("Converted int16 value:", value)
+
+	return nil
+}
